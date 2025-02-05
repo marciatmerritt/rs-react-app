099373825b34cd16f58766c3d5a6ab90192a586c
Marcia Merritt <marciatmerritt@gmail.com>
2025-02-05 16:56:15 -0500

    fix: add stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import './App.css';
-import NavBar from './components/navbar/Navbar';
-import Input from './components/input/Input';
 import Button from './components/button/Button';
+import Input from './components/input/Input';
+import NavBar from './components/navbar/Navbar';
+import Results from './components/results/Results';
+
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <NavBar>
-        <Input type="text" className="search-input" placeholder="Search" />
-        <Button className="search-button">SEARCH</Button>
-      </NavBar>
-    </>
-  );
+	return (
+		<div className='container'>
+			<NavBar>
+				<Input type='text' className='search-input' placeholder='Search' />
+				<Button className='search-button'>SEARCH</Button>
+			</NavBar>
+			<div className='main'>
+				<Results />
+			</div>
+		</div>
+	);
 }
 
 export default App;
